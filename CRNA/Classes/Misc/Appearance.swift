c315046cdd686ed971d8854153111b8be41361da
akonst17 <akonst17@gmail.com>
2022-08-09 16:54:06 +0300

    обновления Nursing; build 4

--- CRNA/Classes/Misc/Appearance.swift
+++ CRNA/Classes/Misc/Appearance.swift
@@ -8,18 +8,14 @@
 import UIKit
 
 struct Appearance {
-    static let backgroundColor = UIColor(integralRed: 247, green: 252, blue: 252)
-    
     static let mainColor = UIColor(integralRed: 92, green: 158, blue: 142)
     static let mainColorAlpha = UIColor(integralRed: 92, green: 158, blue: 142, alpha: 0.3)
-    
-    static let takeFreeTestColor = UIColor(integralRed: 145, green: 203, blue: 222)
-    
-    static let progress1Color = UIColor(integralRed: 92, green: 158, blue: 142)
-    static let progress2Color = UIColor(integralRed: 145, green: 203, blue: 222)
-    static let progress3Color = UIColor(integralRed: 239, green: 191, blue: 94)
-    
+    static let secondColor = UIColor(integralRed: 239, green: 191, blue: 94)
+    static let thirdColor = UIColor(integralRed: 145, green: 203, blue: 222)
     static let successColor = UIColor(integralRed: 46, green: 190, blue: 161)
     static let errorColor = UIColor(integralRed: 255, green: 115, blue: 99)
     static let warningColor = UIColor(integralRed: 255, green: 155, blue: 38)
+    static let backgroundColor = UIColor(integralRed: 247, green: 252, blue: 252)
+    static let greyColor = UIColor(integralRed: 75, green: 81, blue: 102)
+    static let blackColor = UIColor(integralRed: 19, green: 24, blue: 42)
 }
--- CRNA/Classes/Misc/GlobalDefinitions.swift
+++ CRNA/Classes/Misc/GlobalDefinitions.swift
@@ -17,6 +17,7 @@ final class GlobalDefinitions {
     static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
     static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
 
+    static let appStoreUrl = "https://apps.apple.com/app/crna-exam/id1623172590"
     static let appleAppID = "1623172590"
 
     static let termsOfServiceUrl = "https://quantresear.ch/terms.html"
